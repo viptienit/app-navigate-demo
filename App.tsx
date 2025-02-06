1d869f80d6f41703f03c3a736e4bccd8c8687d2b
viptienit <viptienit@gmail.com>
2025-02-06 21:32:58 +0700

    app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Login from "./components/Login";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./components/Home";
 
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="login"
+          component={Login}
+          options={{ title: "Đăng Nhập" }}
+        />
+        <Stack.Screen
+          name="home"
+          component={Home}
+          options={{ title: "Trang chủ" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Home.tsx
@@ -0,0 +1,6 @@
+import { Text } from "react-native";
+
+const Home = () => {
+  return <Text>Home Page</Text>;
+};
+export default Home;
